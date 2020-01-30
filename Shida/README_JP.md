3a04d50a65976f38c38d06dea4a9f8b83a8c2b34
GaraSharp <natural966@live.jp>
2020-01-30 18:55:18 +0900

    adds Font file searching.

--- Shida/README_JP.md
+++ Shida/README_JP.md
@@ -8,17 +8,33 @@ Vでシダの葉グラフィクスを表示するコードを作成しました�
 
 https://ja.wikipedia.org/wiki/%E5%8F%8D%E5%BE%A9%E9%96%A2%E6%95%B0%E7%B3%BB
 
-glfwを使った、グラフィクス表示の勉強の一環で作成しました。
+glfwを使った、グラフィクス表示のお勉強の一環で作成しました。
 
 september.plan によると、glfwの文字表示が国際対応になっているので、
 
-+ fix non-ascii rendering in gg (ä, å, etc)
+- fix non-ascii rendering in gg (ä, å, etc)
 
 ここでは、日本語で文字列を表示しています。
 
-しかし、通常は同梱されている「RobotoMono-Regular.ttf」を利用するので、このままでは日本語は表示されません。
-そこで、手元にダウンロードしておいた「源ノ角ゴシック (Source Han Sans JP Medium) フォント」をRobotoMono-Regular.ttfとリネームして使ってみました。
-他の日本語フォントでも表示できると思います。
+~~しかし、通常は同梱されている「RobotoMono-Regular.ttf」を利用するので、このままでは日本語は表示されません。~~
+~~そこで、手元にダウンロードしておいた「源ノ角ゴシック (Source Han Sans JP Medium) フォント」をRobotoMono-Regular.ttfとリネームして使ってみました。~~
+~~他の日本語フォントでも表示できると思います。​~~
+
+Arch Linuxでは、標準の日本語フォントがないので、以下からダウンロードしたフォント「NotoSansCJKjp-Regular.otf」を利用しております。
+
+noto-cjk/NotoSansCJKjp-Regular.otf 
+https://github.com/googlefonts/noto-cjk/blob/master/NotoSansCJKjp-Regular.otf
+
+ユーザーサイドにフォントを導入すると、~/.local/share/fonts/ に導入されるので、Archではここを参照しております。
+
+Ubuntuでは、標準でNotoフォントが利用できるので、/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc として参照しております。
+
+他のOSの場合、ソースコードと同じディレクトリにあろう、次のフォントを参照するようにしております。
+
++ 'NotoSansCJKjp-Regular.otf' ,
++ 'DroidSerif-Regular.ttf',
+
+「DroidSerif-Regular.ttf」には日本語が入っていないので、何も表示されません。あしからず。
 
 2019-10-18 20:10:51 
 追記 : CのRAND_MAX が C.RAND_MAX として参照できるので、ソースを更新しました。
--- Shida/Shida_no_Ha.v
+++ Shida/Shida_no_Ha.v
@@ -2,9 +2,11 @@
 // begin : 2019-09-06 21:38:31
 // note  : draw シダの葉 in V
 // base  : biomorph.v
+
 // Copyright (c) 2019 Alexander Medvednikov. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
+
 import rand
 import time
 import gx
@@ -12,194 +14,221 @@ import gl
 import gg
 import glfw
 import freetype
+import os
 
 const (
 // Window coords
-	X_min = -3.0
-	X_max = 3.0
-	Y_min = -0.5
-	Y_max = 11.0
-	BlockSize = 2
-	WinWidth = 1150 // window size
-	WinHeight = 600
-	TimerPeriod = 2500 // ms
-	BakColour = 240
-	IterCount = 500000
+    X_min = -3.0
+    X_max =  3.0
+    Y_min = -0.5
+    Y_max = 11.0
+    BlockSize = 2
+    WinWidth  = 1150 // window size
+    WinHeight = 600
+    TimerPeriod = 2000 // ms
+    BakColour = 240
+    IterCount = 500000
 )
 // addition on 2019-09-17 03:12:04
 const (
-	TextSize = 12
+    TextSize = 12
 )
 
 const (
-	text_cfg = gx.TextCfg{
-		align: gx.ALIGN_LEFT
-		size: TextSize
-		color: gx.rgb(0, 0, 0)
-	}
+    text_cfg = gx.TextCfg{
+        align: gx.ALIGN_LEFT
+        size: TextSize
+        color: gx.rgb(0, 0, 0)
+    }
 )
 // Fern graphics canvas
 struct Graph {
 mut:
 // graphics pic-cell array
-	cells       [][]int
-	// gg context for drawing
-	gg          &gg.GG
-	// ft context for font drawing
-	ft          &freetype.FreeType
-	font_loaded bool
+    cells       [][]int
+    // gg context for drawing
+    gg          &gg.GG
+    // ft context for font drawing
+    ft          &freetype.FreeType
+    font_loaded bool
 }
 
 fn main() {
-	glfw.init_glfw() // change on 2019-10-12 19:49:38
-	mut graph := &Graph{
-		gg: gg.new_context(gg.Cfg{
-			// graphix contexts
-			width: WinWidth
-			height: WinHeight
-			use_ortho: true // This is needed for 2D drawing
-			
-			create_window: true
-			window_title: 'Recursive Fern graphics with V'
-			window_user_ptr: graph
-		})
-		ft: 0 // evasive warning : pointer field `Graph*.ft` must be initialized
-		
-	}
-	println('window size : $WinWidth x $WinHeight')
-	graph.gg.window.set_user_ptr(graph) // TODO remove this when `window_user_ptr:` works
-	graph.gg.window.onkeydown(key_down) // MEMO : key event set
-	// Try to load font
-	graph.ft = freetype.new_context(gg.Cfg{
-		width: WinWidth
-		height: WinHeight
-		use_ortho: true
-		font_size: 30
-		scale: 2
-		window_user_ptr: graph // evasive warning : pointer field `gg.Cfg.window_user_ptr` must be initialized
-		
-	})
-	graph.font_loaded = (graph.ft != 0)
-	graph.generate()
-	go graph.run() // Run the graph loop in a new thread
-	gl.clear_color(BakColour, BakColour, BakColour, 255)
-	gl.clear()
-	graph.gg.render()
-	// MEMO : main loop ; Window Realize, Map, and Quit
-	for {
-		gl.clear()
-		gl.clear_color(BakColour, BakColour, BakColour, 255)
-		// clear and draw graph
-		graph.draw_scene()
-		// render() で画像表示とイベント待ち。らしい ?
-		graph.gg.render()
-		if graph.gg.window.should_close() {
-			graph.gg.window.destroy()
-			return
-		}
-		time.sleep_ms(TimerPeriod)
-		// println('Sleep !')
-	}
+    // search font places
+    // Ubuntu (system wide), Arch (user side), etc ...  
+    fils := [
+        '/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc', 
+        os.getenv('HOME')+'/.local/share/fonts/NotoSansCJKjp-Regular.otf' ,
+        os.getenv('HOME')+'/Library/Fonts/NotoSansCJKjp-Regular.otf',
+        '/System/Library/Fonts/ヒラギノ角ゴシック W3.ttc' ,
+        'NotoSansCJKjp-Regular.otf' ,
+        'DroidSerif-Regular.ttf',
+    ]
+
+    //  font searching
+    mut jp_font := ''
+    for f in fils {
+        print('font $f - ')
+        println(os.exists(f))
+        if os.exists(f) { 
+            jp_font = f
+            println('Get font file Nooow !')
+            break 
+        }
+    }
+
+    glfw.init_glfw() // change on 2019-10-12 19:49:38
+
+    mut graph := &Graph{
+        gg: gg.new_context(gg.Cfg{
+            // graphix contexts
+            width:  WinWidth
+            height: WinHeight
+            use_ortho: true // This is needed for 2D drawing
+            
+            create_window: true
+            window_title: 'Recursive Fern graphics with V'
+            window_user_ptr: graph
+        })
+        ft: 0 // evasive warning : pointer field `Graph*.ft` must be initialized
+        
+    }
+    println('window size : $WinWidth x $WinHeight')
+    graph.gg.window.set_user_ptr(graph) // TODO remove this when `window_user_ptr:` works
+    graph.gg.window.onkeydown(key_down) // MEMO : key event set
+    // Try to load font
+    graph.ft = freetype.new_context(gg.Cfg{
+        font_path: jp_font
+        width: WinWidth
+        height: WinHeight
+        use_ortho: true
+        font_size: 30
+        scale: 2
+        window_user_ptr: graph // evasive warning : pointer field `gg.Cfg.window_user_ptr` must be initialized
+        
+    })
+    graph.font_loaded = (graph.ft != 0)
+    graph.generate()
+    go graph.run() // Run the graph loop in a new thread
+    gl.clear_color(BakColour, BakColour, BakColour, 255)
+    gl.clear()
+    graph.gg.render()
+    // MEMO : main loop ; Window Realize, Map, and Quit
+    for {
+        gl.clear()
+        gl.clear_color(BakColour, BakColour, BakColour, 255)
+        // clear and draw graph
+        graph.draw_scene()
+        // render() で画像表示とイベント待ち。らしい ?
+        graph.gg.render()
+        if graph.gg.window.should_close() {
+            graph.gg.window.destroy()
+            return
+        }
+        time.sleep_ms(TimerPeriod)
+        // println('Sleep !')
+    }
 }
 
 // no used ...
 fn (g &Graph) init_graph() {
-	rand.seed(time.now().unix)
+    rand.seed(time.now().unix)
 }
 
 // cell array generates
 fn (g mut Graph) generate() {
-	print('generate, ')
-	// initialize cell space
-	for i := 0; i < WinWidth; i++ {
-		g.cells << [0].repeat(WinHeight)
-	}
-	mut x := f64(0)
-	mut y := f64(0)
-	mut px := f64(0)
-	mut py := f64(0)
-	mut cnt := 0
-	for cnt < IterCount {
-		cnt++
-		// C.RAND_MAX means RAND_MAX in C
-		r := f64(rand.next(C.RAND_MAX)) / C.RAND_MAX
-		if r < 0.01 {
-			x = 0.0
-			y = 0.16 * py
-		}
-		if r >= 0.01 && r < 0.07 {
-			x = 0.2 * px - 0.26 * py
-			y = 0.23 * px + 0.22 * py + 1.6
-		}
-		if r >= 0.07 && r < 0.15 {
-			x = -0.15 * px + 0.28 * py
-			y = 0.26 * px + 0.24 * py + 0.44
-		}
-		if r >= 0.15 {
-			x = 0.85 * px + 0.04 * py
-			y = -0.04 * px + 0.85 * py + 1.6
-		}
-		px = x
-		py = y
-		/*  set pic-cell colour on  */
+    print('generate, ')
+    // initialize cell space
+    for i := 0; i < WinWidth; i++ {
+        g.cells << [0].repeat(WinHeight)
+    }
+    mut x := f64(0)
+    mut y := f64(0)
+    mut px := f64(0)
+    mut py := f64(0)
+    mut cnt := 0
+    for cnt < IterCount {
+        cnt++
+        // C.RAND_MAX means RAND_MAX in C
+        r := f64(rand.next(C.RAND_MAX)) / C.RAND_MAX
+        if r < 0.01 {
+            x = 0.0
+            y = 0.16 * py
+        }
+        if r >= 0.01 && r < 0.07 {
+            x = 0.2 * px - 0.26 * py
+            y = 0.23 * px + 0.22 * py + 1.6
+        }
+        if r >= 0.07 && r < 0.15 {
+            x = -0.15 * px + 0.28 * py
+            y = 0.26 * px + 0.24 * py + 0.44
+        }
+        if r >= 0.15 {
+            x = 0.85 * px + 0.04 * py
+            y = -0.04 * px + 0.85 * py + 1.6
+        }
+        px = x
+        py = y
+        /*  set pic-cell colour on  */
 
-		i := int((py - Y_min) / (Y_max - Y_min) * WinWidth)
-		j := WinHeight - int((X_max - px) / (X_max - X_min) * WinHeight)
-		mut tmp := g.cells[i]
-		tmp[j] = 1
-	}
-	println('generated ')
+        i := int((py - Y_min) / (Y_max - Y_min) * WinWidth)
+        j := WinHeight - int((X_max - px) / (X_max - X_min) * WinHeight)
+        mut tmp := g.cells[i]
+        tmp[j] = 1
+    }
+    println('generated ')
 }
 
 // MEMO : main graph loop thread
 fn (g &Graph) run() {
-	for {
-		glfw.post_empty_event() // force window redraw
-		time.sleep_ms(TimerPeriod)
-	}
+    for {
+        glfw.post_empty_event() // force window redraw
+        time.sleep_ms(TimerPeriod)
+    }
 }
 
 fn (g &Graph) draw_curve() {
-	for j := 0; j < WinHeight; j++ {
-		for i := 0; i < WinWidth; i++ {
-			tmp := g.cells[i]
-			if tmp[j] == 1 {
-				g.gg.draw_rect(i, j, BlockSize - 1, BlockSize - 1, gx.rgb(10, 100, 10))
-			}
-		}
-	}
+    for j := 0; j < WinHeight; j++ {
+        for i := 0; i < WinWidth; i++ {
+            tmp := g.cells[i]
+            if tmp[j] == 1 {
+                g.gg.draw_rect(i, j, BlockSize - 1, BlockSize - 1, gx.rgb(10, 100, 10))
+            }
+        }
+    }
 }
 
 fn (g mut Graph) draw_message() {
-	if g.font_loaded {
-		g.ft.draw_text(100, 50, 'シダの葉グラフィクス', text_cfg)
-	}
+    if g.font_loaded {
+        g.ft.draw_text(100, 50, 'シダの葉グラフィクス', text_cfg)
+    }
 }
 
 fn (g mut Graph) draw_scene() {
-	g.draw_curve()
-	g.draw_message()
+    g.draw_curve()
+    g.draw_message()
 }
 
 // 以下のkey_down() では、キー入力で、ESCキーの場合に
 // glfw.set_shoulkd_close を true にする、らしい
 // TODO: this exposes the unsafe C interface, clean up
 fn key_down(wnd voidptr, key, code, action, mods int) {
-	println('key_down()')
-	// debug ; println('action = $action')
-	// action : key-in=1, key-release=0, key-repeat=2
-	// so, follow line is キーリピートでもなく、キー押下でもない、
-	// キー・リリースの場合には[戻る]、ということらしい
-	if action != 2 && action != 1 {
-		return
-	}
-	// Fetch the graph object stored in the user pointer
-	match key {
-		glfw.KEY_ESCAPE {
-			// case GLFW_KEY_ESCAPE:
-			glfw.set_should_close(wnd, true)
-			println('key detections')
-		}
-		else {}
-	}
+    println('key_down()')
+    // debug ; println('action = $action')
+    // action : key-in=1, key-release=0, key-repeat=2
+    // so, follow line is キーリピートでもなく、キー押下でもない、
+    // キー・リリースの場合には[戻る]、ということらしい
+    if action != 2 && action != 1 {
+        return
+    }
+    // Fetch the graph object stored in the user pointer
+    match key {
+        glfw.KEY_ESCAPE {
+            // case GLFW_KEY_ESCAPE:
+            glfw.set_should_close(wnd, true)
+            println('key detections')
+        }
+        else {}
+    }
 }
+
